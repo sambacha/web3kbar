407d7398b197cce13aaa7ace84084cfb5553ff48
sam bacha <sam@manifoldfinance.com>
2021-11-11 13:16:10 -0800

    rework homepage and modal

--- containers/Home/Home.tsx
+++ containers/Home/Home.tsx
@@ -7,10 +7,25 @@ const Home: FunctionComponent = () => {
   return (
     <div className="mt-20">
       <h1 className="text-5xl font-bold">Web3 🍣Bar</h1>
+      <p> </p>
       <h2 className="mt-12 mb-8 text-xl">
-        Web3 DApps <span className="line-through"> search ba r</span> 🍣 Bar
+        Web3 DApps <span className="line-through">search bar</span> 🍣 Bar
       </h2>
-      <h3 className="text-xl"> type `CMD+K` then `T + 1` to get the latest trending asset (24hrs) </h3>
+      <br />
+      
+      <h3 className="text-xl"> type <kbd>CMD</kbd> + <kbd> K </kbd>  
+      <br />
+      type <kbd>T</kbd> + <kbd> 1 </kbd> to get the latest trending asset (24hrs)</h3>
+
+<br />
+<h4>web3Bar Emoji Maps</h4>
+      <p>
+      🍱 - Exchange  <br />
+      🍣 - Bar  <br />
+      🐳 - whale activity  <br />
+      🚜 - farming/harvest  <br />
+      🔥 - heavy trading  <br />
+      </p>
     </div>
 
     
